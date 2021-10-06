d5b0bea3d646df9e52c411fe71913b63b0ba199e
Rachel Schwartz <rahelsc@gmail.com>
2021-10-06 12:06:30 +0300

    share done

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,11 +1,14 @@
-import "./feed.css"
+import Share from "../share/Share";
+import "./feed.css";
 
 const Feed = () => {
-    return (
-        <div className="feedContainer">
-            <div className="feedWrapper"></div>
-        </div>
-    )
-}
+  return (
+    <div className="feedContainer">
+      <div className="feedWrapper">
+          <Share/>
+      </div>
+    </div>
+  );
+};
 
-export default Feed
+export default Feed;
